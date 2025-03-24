9a0369cafd0c10883bf581f507ae10b7f50ea9ec
Tristan Goel <trgoel@c2r9s5.42lausanne.ch>
2025-03-24 17:12:37 +0100

    should work

---
+++ module02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedFloatingPts(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Default Destructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(const Fixed &old) : _fixedFloatingPts(old.getRawBits())
+{
+	std::cout << "Copy constructor called" << std::endl;
+	return ;
+}
+
+int		Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedFloatingPts);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedFloatingPts = raw;
+	return ;
+}
+
+Fixed	&Fixed::operator=(const Fixed &old)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_fixedFloatingPts = old.getRawBits();
+	return (*this);
+}
---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,19 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int					_fixedFloatingPts;
+		static const int	bits = 8;
+	public:
+		Fixed();
+        Fixed(const Fixed &old);
+		~Fixed(void);
+		int		getRawBits( void ) const;
+		void	setRawBits(int const raw);
+        Fixed   &operator=(const Fixed &old);
+};
+
+#endif
