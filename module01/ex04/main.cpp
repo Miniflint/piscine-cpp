dc4b9e9b1937a61062c3d8ace55c6268eecc7306
Tristan Goel <trgoel@c1r3s5.42lausanne.ch>
2025-05-28 08:48:53 +0200

    renamed main.c to main.cpp

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -5,11 +5,14 @@ int main(int argc, char **argv)
     if (argc != 4)
         return ((std::cout << "Usage:" << argv[0] << " <file> <search> <replace>" << std::endl), 1);
     std::ifstream   inFile(argv[1]);
-    if (!inFile.is_open())
+    if (!inFile.is_open()) {
         return ((std::cout << "inFile does not open/exist" << std::endl), 1);
-    std::ofstream   outFile(std::string(argv[1]) + ".replace");
-    if (!outFile.is_open())
+	}
+	std::string filename = std::string(argv[1]) + ".replace";
+    std::ofstream   outFile(filename.c_str());
+    if (!outFile.is_open()) {
         return ((std::cout << "outFile does not open/exist" << std::endl), 1);
+	}
     std::string     s1 = argv[2];
     std::string     s2 = argv[3];
     std::string     cText;
@@ -22,7 +25,7 @@ int main(int argc, char **argv)
         while (1)
         {
             i = cText.find(s1, i + iSize);
-            if (i == std::string::npos)
+            if (i == (long int)(std::string::npos))
                 break ;
             cText = cText.substr(0, i) + s2 + cText.substr(i + iSize, cText.size() - (i + iSize));
         }
